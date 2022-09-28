935d2275d28e77a984e7fed03cf93b457c2c116d
rexcom28 <franciscojavierrod28@gmail.com>
2022-09-28 13:16:54 -0600

    Changes addes to am command

--- petnet/urls.py
+++ petnet/urls.py
@@ -1,9 +1,14 @@
+from xml.etree.ElementInclude import include
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from core.views import frontpage, about
 urlpatterns = [
-    path('', frontpage, name='frontpage'),
-    path('about/', about, name='about'),
     path('admin/', admin.site.urls),
+    path('', include('store.urls')),
+    path('', frontpage, name='frontpage'),    
+    path('about/', about, name='about'),
+
+    
+    
     
 ]
--- store/views.py
+++ store/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+def product_detail(request, slug):
+    product = Product.objects.get(slug=slug)
+    
+    return render(request, 'store/product_detail.html', {
+        'product':product
+    })
