3e811261d73c4ee34215fe2dc100bd07d8e53363
rexcom28 <franciscojavierrod28@gmail.com>
2022-09-30 12:06:24 -0600

    Category, menu tag functionality

---
+++ store/templatetags/menu.py
@@ -0,0 +1,11 @@
+from atexit import register
+import imp
+from django import template
+
+from store.models import Category
+register = template.Library()
+
+@register.inclusion_tag('core/menu.html')
+def menu():
+    categories = Category.objects.all()
+    return {'categories': categories}
