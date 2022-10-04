a60fcc0b5bb80e8b5105aab158237b11cdaaa7a6
rexcom28 <franciscojavierrod28@gmail.com>
2022-10-04 11:53:38 -0600

    Add, edit product functionality added

---
+++ store/forms.py
@@ -0,0 +1,10 @@
+from dataclasses import field
+from django import forms
+from django import forms
+from . models import Product
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ('category', 'title', 'description', 'price', 'image',)
+        
--- userprofile/views.py
+++ userprofile/views.py
@@ -1,9 +1,14 @@
 
 from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.shortcuts import redirect, render
+from django.utils.text import slugify
 from . models import Userprofile
+
+from store.forms import ProductForm
+from store.models import Product
 
 def vendor_detail(request, pk):
     user = User.objects.get(pk=pk)
@@ -11,6 +16,49 @@ def vendor_detail(request, pk):
         'user':user
     })
 
+@login_required
+def my_store(request):
+    return render(request, 'userprofile/my_store.html')
+
+@login_required
+def add_product(request):
+    print(',m', request.method )
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        print(form.is_valid)
+        if form.is_valid():
+            title = request.POST.get('title')            
+            product = form.save(commit=False)
+            product.user = request.user
+            product.slug = slugify(title)
+            product.save()
+            return redirect ('my_store')
+    else:
+        form = ProductForm()
+    
+    return render(request, 'userprofile/add_product.html',{
+        'title':'Add product',
+        'form':form
+    })
+
+@login_required
+def edit_product(request, pk):
+    product = Product.objects.filter(user=request.user).get(pk=pk)
+
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            return redirect ('my_store')
+    else:    
+        form = ProductForm(instance=product)
+
+    return render(request, 'userprofile/add_product.html',{
+        'title':'Edit product',
+        'form':form
+    })
+
+@login_required
 def myaccount(request):
     return render(request, 'userprofile/myaccount.html')
   
