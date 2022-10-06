5e5053ea5dc99ed50bddc9de2f8cbe44ad20fb20
rexcom28 <franciscojavierrod28@gmail.com>
2022-10-06 10:02:24 -0600

    component profile tailwind

--- userprofile/models.py
+++ userprofile/models.py
@@ -1,8 +1,10 @@
+from email.policy import default
 from django.db import models
 from django.contrib.auth.models import User
 
 class Userprofile(models.Model):
     user = models.OneToOneField(User, related_name='userprofile', on_delete= models.CASCADE)
+    is_vendor =models.BooleanField(default=False)
     
     def __str__(self):
         return self.user.username
