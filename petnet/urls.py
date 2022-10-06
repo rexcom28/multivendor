5fa91ebecd369b95e2679276c65953ff717086c5
rexcom28 <franciscojavierrod28@gmail.com>
2022-10-06 12:31:51 -0600

    update

--- petnet/urls.py
+++ petnet/urls.py
@@ -12,4 +12,8 @@ urlpatterns = [
     path('', include('store.urls')),
     path('', frontpage, name='frontpage'),
     
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+if settings.DEBUG:
+    urlpatterns +=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns +=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)   
+       
