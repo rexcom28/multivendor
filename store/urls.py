32f746812f2f02fa741b75674437bdf21e8d7d5e
rexcom28 <franciscojavierrod28@gmail.com>
2022-10-12 07:53:22 -0600

    succes

--- store/urls.py
+++ store/urls.py
@@ -5,6 +5,7 @@ urlpatterns =[
     path('add-to-cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
     path('cart/', views.cart_view, name='cart_view'),
     path('cart/checkout/',views.checkout, name="checkout"),
+    path('cart/success/', views.success, name='success'),
     path('change-quantity/<str:product_id>', views.change_quantity, name='change_quantity'),
     path('remove-from-cart/<str:product_id>/',views.remove_from_cart, name='remove_from_cart'),
     path('<slug:slug>/', views.category_detail, name='category_detail'),
--- store/views.py
+++ store/views.py
@@ -19,6 +19,9 @@ def add_to_cart(request, product_id):
     cart.add(product_id)
     return redirect('cart_view')
 
+def success(request):
+    return render(request, 'store/success.html')
+
 def remove_from_cart(request, product_id):
     cart = Cart(request)
     cart.remove(product_id)
@@ -34,57 +37,68 @@ def cart_view(request):
 @login_required
 def checkout(request):
     cart = Cart(request)
+    if cart.get_total_cost() == 0:
+        return redirect('cart_view')
     
     if request.method == 'POST':
+        data = json.loads(request.body)
+        
         form = OrderForm(request.POST)
         
-        if form.is_valid():
-            total_price =0
-            
-            items = []
-            
-            for item in cart:
-                product= item['product']
-                total_price += product.price * int(item['quantity'])
-                items.append({
-                    'price_data':{
-                        'currency': 'usd',
-                        'product_data':{
-                            'name': product.title,
-                        },
-                        'unit_amount': product.price
+        
+        total_price =0
+        
+        items = []
+        
+        for item in cart:
+            product= item['product']
+            total_price += product.price * int(item['quantity'])
+            items.append({
+                'price_data':{
+                    'currency': 'usd',
+                    'product_data':{
+                        'name': product.title,
                     },
-                    'quantity':item['quantity']
-                })
+                    'unit_amount': product.price
+                },
+                'quantity':item['quantity']
+            })
+        
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        session = stripe.checkout.Session.create(
+            payment_method_types=['card'],
+            line_items=items,
+            mode='payment',
+            success_url=f'{settings.WEB_SITE_URL}cart/success/',
+            cancel_url =f'{settings.WEB_SITE_URL}cart/'
             
-            stripe.api_key = settings.STRIPE_SECRET_KEY
-            session = stripe.checkout.Session.create(
-                payment_method_types=['card'],
-                line_items=items,
-                mode='payment',
-                success_url='http:127.0.0.1:8000/cart/success/',
-                cancel_url ='http:127.0.0.1:8000/cart/'
-                
-            )
-            payment_intent = session.payment_intent
-            order = form.save(commit=False)
-            order.created_by = request.user
-            order.is_paid = True
-            order.payment_intent = payment_intent
-            order.paid_amount = total_price
-            order.save()
+        )
+        payment_intent = session.payment_intent
+        
+        order = Order.objects.create(
+            first_name = data['first_name'],
+            last_name  = data['last_name'],
+            address    = data['address'],
+            zipcode    = data['zipcode'],
+            city       = data['city'],
+            created_by = request.user,
+            is_paid = True,
+            payment_intent = payment_intent,
+            paid_amount = total_price
+        )
+        
+        
+        #create order
+        for item in cart:
+            product = item['product']
+            quantity = int(item['quantity'])
+            price = product.price * quantity
             
-            #create order
-            for item in cart:
-                product = item['product']
-                quantity = int(item['quantity'])
-                price = product.price * quantity
-                
-                item = OrderItem.objects.create(order=order, product=product, price=price, quantity=quantity)
-                
-            cart.clear()    
+            item = OrderItem.objects.create(order=order, product=product, price=price, quantity=quantity)
             
-            return JsonResponse({'session':session, 'order':payment_intent}) #redirect('myaccount')
+        cart.clear()    
+        
+        return JsonResponse({'session':session, 'order':payment_intent}) #redirect('myaccount')
     else:    
         form = OrderForm()
     return render(request, 'store/checkout.html', {
