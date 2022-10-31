22c6995b9ea76eec0384031de2d0d93ed61627b1
rexcom28 <franciscojavierrod28@gmail.com>
2022-10-31 09:18:01 -0700

    decorator.py

--- store/decorator.py
+++ store/decorator.py
@@ -2,10 +2,12 @@
 from django.core.exceptions import PermissionDenied
 
 def check_user_able_to_see_page(*groups):
-
+    print('groupssss',groups)
     def decorator(function):
+        print('-----fucnc name--------',function.__name__)
         def wrapper(request, *args, **kwargs):
-            if request.user.groups.filter(name__in=groups).exists():
+            print('re,args,kwgars',request, args,kwargs)
+            if request.user.groups.filter(name__in=groups).exists() | request.user.is_superuser:
                 return function(request, *args, **kwargs)
             #raise Http404
             raise PermissionDenied
